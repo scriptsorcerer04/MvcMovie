f43d3080ba30850bfd9320eda2bd0c4cd36f3f91
scriptsorcerer04 <morgancorey61@gmail.com>
2024-04-12 17:36:25 +0300

    Added a new controller

--- MvcMovie/obj/Debug/net6.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net6.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce75b2c24037f6097d55df954bb51d76dc92bf9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
